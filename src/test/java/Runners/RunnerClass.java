925845537e47917ee6101c817a5307ea7ca4e616
sohai <SyntaxTechnologies>
2022-01-16 00:16:11 +0530

    Class3

--- src/test/java/Runners/RunnerClass.java
+++ src/test/java/Runners/RunnerClass.java
@@ -8,13 +8,17 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         //features we use to provide the path of all the feature files
-        features = "src/test/resources/features/Login.feature",
+        features = "src/test/resources/features/AddEmployee.feature",
         //glue is where we find implementations for gherkin steps
         //we provide the path of package where we defined all the steps
         glue = "steps",
         //if we set it to true, it will quickly scan all gherkin steps whether they are implemented or not
         //if it is set to true, it stops actual execution
-        dryRun = false
+        //to execute script, it should be set to false
+        dryRun = false,
+        //it means the console output for cucumber test is having some irrelavant information
+        //when we set it to true, it simply removes all the irrelevant information from the console
+        monochrome = true
 
 )
 
